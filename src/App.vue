ec723f4ee45e7ac281fa74b47599395cbb4f1fd3
XJ王大哥 <xjwangdage@163.com>
2025-08-05 14:53:42 +0800

    6

--- src/App.vue
+++ src/App.vue
@@ -3,32 +3,30 @@
 </script>
 
 <template>
-  <t-typography-title>欢迎来到 XJ の 神秘主页</t-typography-title>
-  <t-head-menu v-model="menu1Value" theme="light" @change="changeHandler">
-    <template #logo>
-      <img height="28" src="https://tdesign.gtimg.com/site/baseLogo-light.png" alt="logo" />
-    </template>
-    <t-menu-item value="item1"> 菜单1 </t-menu-item>
-    <t-menu-item value="item2"> 菜单2 </t-menu-item>
-    <t-menu-item value="item4" :disabled="true"> 禁用菜单 </t-menu-item>
-    <template #operations>
-      <t-button variant="text" shape="square">
-        <template #icon><t-icon name="search" /></template>
-      </t-button>
-      <t-button variant="text" shape="square">
-        <template #icon><t-icon name="mail" /></template>
-      </t-button>
-      <t-button variant="text" shape="square">
-        <template #icon><t-icon name="user" /></template>
-      </t-button>
-      <t-button variant="text" shape="square">
-        <template #icon><t-icon name="ellipsis" /></template>
-      </t-button>
-    </template>
-  </t-head-menu>
+    <t-typography-title>欢迎来到 XJ の 神秘主页</t-typography-title>
+    <t-head-menu theme="light">
+        <template #logo>
+            <img height="28" src="https://tdesign.gtimg.com/site/baseLogo-light.png" alt="logo" />
+        </template>
+        <t-menu-item value="item1"> 菜单1 </t-menu-item>
+        <t-menu-item value="item2"> 菜单2 </t-menu-item>
+        <t-menu-item value="item4" :disabled="true"> 禁用菜单 </t-menu-item>
+        <template #operations>
+            <t-button variant="text" shape="square">
+                <template #icon><t-icon name="search" /></template>
+            </t-button>
+            <t-button variant="text" shape="square">
+                <template #icon><t-icon name="mail" /></template>
+            </t-button>
+            <t-button variant="text" shape="square">
+                <template #icon><t-icon name="user" /></template>
+            </t-button>
+            <t-button variant="text" shape="square">
+                <template #icon><t-icon name="ellipsis" /></template>
+            </t-button>
+        </template>
+    </t-head-menu>
 </template>
 
 
-<style scoped>
-
-</style>
+<style scoped></style>
